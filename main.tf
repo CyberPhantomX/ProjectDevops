4da65d12e07313b35b6e09b0bc02b03b0cf1f2df
CyberPhantomX <151735432+CyberPhantomX@users.noreply.github.com>
2024-04-07 21:00:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_security_group" "ProjeDevops-sg" {
-  name        = "ProjetDevops Security Group"
+resource "devOps Security Group" "devOps-sg" {
+  name        = "devOps Security Group"
   description = "Open 22,443,80,8080,9090,3000"
   ingress = [
-    	for port in [22, 80, 443, 8080 ,9090,3000] : {
+    	for port in [22,443,80,8080,9090,3000] : {
       description      = "TLS from VPC"
       from_port        = port
       to_port          = port
@@ -21,7 +21,7 @@ resource "aws_security_group" "ProjeDevops-sg" {
    	 cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "ProjetDevOps-sg"
+    Name = "devOps-sg"
   }
 }
 resource "aws_instance" "Jenkins" {
@@ -42,7 +42,7 @@ resource "aws_instance" "Prometheus-Grafana" {
   ami                    = "ami-043dd7c072304d1db"  #change Ami if you different region
   instance_type          = "t2.micro"
   key_name               = "KeyPairJeremy"
-  vpc_security_group_ids = [aws_security_group.ProjetDevops-sg.id]
+  vpc_security_group_ids = [aws_security_group.devOps-sg.id]
   user_data              = templatefile("./install_prometheus_grafana.sh", {})
   tags = {
     Name = "Prometheus-Grafana"
