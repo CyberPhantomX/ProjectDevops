65b1a8e77494ef0e5a30e7f5a9c54e5e03c68bed
CyberPhantomX <151735432+CyberPhantomX@users.noreply.github.com>
2024-04-07 19:49:43 +0200

    Add files via upload

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 sudo apt update -y
+sudo touch /etc/apt/keyrings/adoptium.asc
+sudo wget -O /etc/apt/keyrings/adoptium.asc https://packages.adoptium.net/artifactory/api/gpg/key/public
 
-sudo useradd --system --no-create-home --shell /bin/false
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
 
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+sudo apt update -y
+sudo apt install temurin-17-jdk -y
 
- echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-    /etc/apt/sources.list.d/jenkins.list > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-sudo apt-get update
-  sudo apt-get install fontconfig openjdk-17-jre
-  sudo apt-get install jenkins
-  
-pub rsa4096 2023-03-27 [SC] [expire : 2026-03-26]
-      63667EE74BBA1F0A08A698725BA31D57EF5975CA
-Projet Jenkins
-sous rsa4096 2023-03-27 [E] [expire : 2026-03-26]
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
