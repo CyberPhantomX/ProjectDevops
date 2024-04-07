f3e6f05e6a0049bc0b963f0d6a2b9500563be541
CyberPhantomX <151735432+CyberPhantomX@users.noreply.github.com>
2024-04-07 21:56:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "ProjectdevOps-sg" {
   }
 }
 resource "aws_instance" "Jenkins" {
-  ami                    = "ami-043dd7c072304d1db"  #change Ami if you different region
+  ami                    = "ami-0facbf2a36e11b9dd"  #change Ami if you different region
   instance_type          = "t2.micro"
   key_name               = "KeyPairJeremy"
   vpc_security_group_ids = [aws_security_group.ProjectdevOps-sg.id]
@@ -39,7 +39,7 @@ resource "aws_instance" "Jenkins" {
 }
 
 resource "aws_instance" "Prometheus-Grafana" {
-  ami                    = "ami-043dd7c072304d1db"  #change Ami if you different region
+  ami                    = "ami-0facbf2a36e11b9dd"  #change Ami if you different region
   instance_type          = "t2.micro"
   key_name               = "KeyPairJeremy"
   vpc_security_group_ids = [aws_security_group.ProjectdevOps-sg.id]
